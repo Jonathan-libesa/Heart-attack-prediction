e99ab93d2152b2ccae03e1f78c1ee2db82bc2952
Jonathan <75207695+Jonathan-libesa@users.noreply.github.com>
2023-11-08 11:33:51 +0300

    Add files via upload

--- analysis.py
+++ analysis.py
@@ -3,13 +3,25 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+from sklearn.ensemble import RandomForestClassifier,GradientBoostingClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import RocCurveDisplay
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
 # Read the dataset into a pandas DataFrame
 df = pd.read_csv( r"C:\Users\cash\Documents\SEMSTER 4.1 NOTES\heart.csv")
 
 print(df)
 
+#Changing some column names for easy readability
+new_columns = ["age","sex","cp","trtbps","chol","fbs","rest_ecg","thalach","exang","oldpeak","slope","ca","thal","target"]
+df.columns = new_columns
 
+df.head()
 
 #SHOW INFORMATION FOR INFO
 df.info()
@@ -37,9 +49,9 @@ plt.ylabel('Frequency')
 plt.title('Age Distribution')
 plt.show()
 
-# Explore the distribution of Heart Disease by Gender
+ #Explore the distribution of Heart Disease by Gender
 plt.figure(figsize=(10, 6))
-sns.countplot(x='sex', data=df, hue='output')
+sns.countplot(x='sex', data=df, hue='target')
 plt.title('Distribution of Heart Disease by Gender')
 plt.show()
 
@@ -74,13 +86,125 @@ plt.title('Distribution of Chest Pain Type')
 plt.show()
 
  #Explore the distribution of numerical features
-numeric_features = ['age', 'trtbps', 'chol', 'thalachh', 'oldpeak']
+numeric_features = ['age', 'trtbps', 'chol', 'thalach', 'oldpeak']
 df[numeric_features].hist(bins=15, figsize=(12, 6))
 plt.show()
 
 
 # Explore the distribution of categorical features
 plt.figure(figsize=(12, 6))
-sns.countplot(x='sex', data=df, hue='output')
+sns.countplot(x='sex', data=df, hue='target')
 plt.title('Distribution of Heart Disease by Gender')
 plt.show()
+
+
+
+# Model Preperation:
+df_copy = df.copy()
+df_copy.head()
+
+
+X = df_copy.drop(["target"],axis=1)
+Y = df_copy[["target"]]
+
+X_train,X_test,y_train,y_test=train_test_split(X,Y,test_size=0.1,random_state=3)
+
+# Create and train a logistic regression model
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+# Make predictions on the test set
+y_pred = model.predict(X_test)
+
+
+# Evaluate the model
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Accuracy: {accuracy}')
+print(classification_report(y_test, y_pred))
+
+
+#DecisionTreeClassifier
+
+Tree_model=DecisionTreeClassifier(max_depth=10)
+
+# fit model
+Tree_model.fit(X_train,y_train)
+y_pred_T =Tree_model.predict(X_test)
+
+# Score X and Y - test and train
+print("Score the X-train with Y-train is : ", Tree_model.score(X_train,y_train))
+print("Score the X-test  with Y-test  is : ", Tree_model.score(X_test,y_test))
+print("Model Evaluation Decision Tree : accuracy score " , accuracy_score(y_test,y_pred_T))
+
+
+
+
+# using the model K Neighbors Classifier
+
+K_model = KNeighborsClassifier(n_neighbors = 11)
+K_model.fit(X_train, y_train)
+
+y_pred_k = K_model.predict(X_test)
+
+print("Score the X-train with Y-train is : ", K_model.score(X_train,y_train))
+print("Score the X-test  with Y-test  is : ", K_model.score(X_test,y_test))
+print("Model Evaluation K Neighbors Classifier : accuracy score " , accuracy_score(y_test,y_pred_k))
+
+
+
+
+# using the model Random Forest Classifier
+
+RF_model = RandomForestClassifier(n_estimators = 300)
+RF_model.fit(X_train, y_train)
+
+y_pred_r = RF_model.predict(X_test)
+
+print("Score the X-train with Y-train is : ", RF_model.score(X_train,y_train))
+print("Score the X-test  with Y-test  is : ", RF_model.score(X_test,y_test))
+print("Model Evaluation Random Forest Classifier : accuracy score " , accuracy_score(y_test,y_pred_r))
+
+# ANALYSIS CLASSIFICATION USING RANDOMFORESTCLASSIFIER
+random_forest = RandomForestClassifier(random_state=5)
+random_forest.fit(X_train,y_train)
+y_pred_4 = random_forest.predict(X_test)
+
+print("The test accuracy score using Random Forest Algorithm is:",accuracy_score(y_test,y_pred_4))
+
+
+
+RocCurveDisplay.from_estimator(random_forest, X_test, y_test, name="Random Forest Algorithm")
+plt.title("Random Forest Algorithm Roc Curve and AUC")
+plt.plot([0,1],[0,1],"r--")
+plt.show()
+
+
+
+parameters = {"n_estimators":[50,100,150,200],
+             "criterion":["gini","entropy"],
+             "max_features":["auto","sqrt","log2"],
+             "bootstrap":[True,False]}
+
+
+random_forest_grid = GridSearchCV(random_forest_new,param_grid=parameters)
+random_forest_grid.fit(X_train,y_train)
+
+
+
+print("Best Parameters:",random_forest_grid.best_params_)
+
+
+
+
+random_forest_new2 = RandomForestClassifier(bootstrap=True,criterion="entropy",max_features="auto",n_estimators=200, random_state=5)
+random_forest_new2.fit(X_train,y_train)
+
+
+y_pred_4= random_forest_new2.predict(X_test)
+print("accuracy score of Random Forest after hyper-parameter tuning is:",accuracy_score(y_test,y_pred_4))
+
+
+
+RocCurveDisplay.from_estimator(random_forest_new2, X_test, y_test, name="Random Forest Algorithm")
+plt.title("Random Forest Algorithm Roc Curve and AUC")
+plt.plot([0,1],[0,1],"r--")
